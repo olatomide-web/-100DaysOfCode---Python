e949c7b3cd133ed001f5b6146aea9613f6af071b
olatomide-web <joshboris2020@gmail.com>
2023-01-13 14:49:11 -0800

    #100DaysOfCode

---
+++ quiz-game-start/question_model.py
@@ -0,0 +1,11 @@
+class Question:
+
+    def __init__(self, question_text, question_answer):
+        self.text = question_text
+        self.answer = question_answer
+
+# new_question = Question("balablu", "True")
+# print(new_question.text)
+# print(new_question.answer)
+
+
