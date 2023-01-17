932d4e5d93ba0ec965f17afd89877535f179cbf4
olatomide-web <joshboris2020@gmail.com>
2023-01-17 13:50:59 -0800

    #100DaysOfCode

--- Day20 Snake game/Day-20 Snake Game.py
+++ Day20 Snake game/Day-20 Snake Game.py
@@ -1,7 +1,8 @@
 from turtle import Turtle, Screen
 import time
 from snake import Snake
-
+from food import Food
+from scoreboard import Scoreboard
 screen = Screen()
 screen.setup(width=600, height=600)
 screen.bgcolor("black")
@@ -9,7 +10,8 @@ screen.title("Snake Xenzia")
 screen.tracer(0)
 
 snake = Snake()
-
+food = Food()
+scoreboard = Scoreboard()
 
 screen.listen()
 screen.onkey(snake.up, "Up")
@@ -27,5 +29,28 @@ while game_is_on:
     snake.move()
     
 
+    #Detect collision with food
+
+    if snake.head.distance(food) < 15:
+        food.refresh()
+        snake.extend()
+        scoreboard.increase_score()
+
+
+    #Detect hit with wall
+
+    if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
+        game_is_on = False
+        scoreboard.game_over()
+
+    #Detect collision with tail
+
+    for segment in snake.segments:
+        if segment == snake.head:
+            pass
+        
+        elif snake.head.distance(segment) < 10:
+            game_is_on = False
+            scoreboard.game_over()
 
 screen.exitonclick()
---
+++ Day20 Snake game/scoreboard.py
@@ -0,0 +1,31 @@
+from turtle import Turtle
+ALIGNMENT = "center"
+FONT = ("Arial", 24, "normal")
+class Scoreboard(Turtle):
+
+    def __init__(self):
+        super().__init__()
+        self.score = 0
+        self.color("white")
+        self.penup()
+        self.goto(0, 270)
+        self.write(f"Score: {self.score}", align="center", font=("Arial",24, "normal"))
+        self.hideturtle()
+        self.update_score_board
+
+    def update_score_board(self):
+        self.write(f"Score: {self.score}", align=ALIGNMENT, font=FONT)
+
+
+    def game_over(self):
+        self.goto(0, 0)
+        self.write("Game Over", align=ALIGNMENT, font=FONT)
+
+
+    def increase_score(self):
+        self.score += 1
+        self.clear()
+        self.update_score_board()
+    
+
+
