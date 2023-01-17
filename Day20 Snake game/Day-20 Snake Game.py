5433c63a95747e3e820a7fd567df452809f81c4b
olatomide-web <joshboris2020@gmail.com>
2023-01-16 21:55:49 -0800

    #100DaysOfCode

---
+++ Day20 Snake game/Day-20 Snake Game.py
@@ -0,0 +1,31 @@
+from turtle import Turtle, Screen
+import time
+from snake import Snake
+
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.bgcolor("black")
+screen.title("Snake Xenzia")
+screen.tracer(0)
+
+snake = Snake()
+
+
+screen.listen()
+screen.onkey(snake.up, "Up")
+screen.onkey(snake.down, "Down")
+screen.onkey(snake.left, "Left")
+screen.onkey(snake.right, "Right")
+
+
+
+game_is_on = True
+while game_is_on:
+    screen.update()
+    time.sleep(0.2)
+    
+    snake.move()
+    
+
+
+screen.exitonclick()
